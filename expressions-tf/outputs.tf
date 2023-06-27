48cdb132dd9cd2a7390c0b2f953f2f0cb0549889
mbizon <mbizon@purestorage.com>
2023-06-27 10:55:07 +0200

    expressions lab 16

--- expressions-tf/outputs.tf
+++ expressions-tf/outputs.tf
@@ -3,8 +3,7 @@
 output "nginx_hosts" {
   value = [
     for container in docker_container.nginx : {
-      name : container.name, host :
-        "${container.ports[0].ip}:${container.ports[0].external}"
+      name : container.name, host : "${container.ports[0].ip}:${container.ports[0].external}"
     }
   ]
 }
