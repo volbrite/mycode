b79463b60375715f11ac9f1ab919776251fb8474
mbizon <mbizon@purestorage.com>
2023-06-29 15:14:13 +0200

    lab 37 - go-tf / go-restful-api - challenge 2

--- go-tf/main.tf
+++ go-tf/main.tf
@@ -5,19 +5,11 @@ terraform {
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 2.22.0"
-    },
-    http = {
-      source = "hashicorp/http"
-      version = "3.0.1"
     }
   }
 }
 
 provider "docker" {}
-
-provider "http" {
-  # Configuration options
-}
 
 resource "docker_image" "gosvr" {
   # the name of our go image
@@ -32,22 +24,4 @@ resource "docker_container" "gosvr" {
     internal = 9876
     external = 2224
   }
-}
-
-
-# The following example shows how to issue an HTTP GET request supplying
-# an optional request header.
-data "http" "example" {
-  url = "http://0.0.0.0:9876/spock"
-
-  # Optional request headers
-  request_headers = {
-    Accept = "application/json"
-  }
-}
-
-# produces an output value named "spocks_wisdom"
-output "spocks_wisdom" {
-  description = "response from /spock api"
-  value       = data.http.example.response_body
 }
